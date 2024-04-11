2ec055e341b50d51d380d4221cdc4fb599b3b886
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-11 11:51:54 +0100

    Refactor to allow for new filtering hook, and add the hook.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -2,10 +2,29 @@ import { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { ListedMovie } from "../types/interfaces";
+import useFiltering from "../hooks/useFiltering";
+import MovieFilterUI, {
+  titleFilter,
+  genreFilter,
+} from "../components/movieFilterUI";
+
+const titleFiltering = {
+  name: "title",
+  value: "",
+  condition: titleFilter,
+};
+const genreFiltering = {
+  name: "genre",
+  value: "0",
+  condition: genreFilter,
+};
 
 const UpcomingMoviesPage: React.FC = () => {
   const [upcomingMovies, setUpcomingMovies] = useState<ListedMovie[]>([]);
-
+  const { filterValues, setFilterValues, filterFunction } = useFiltering(
+    [],
+    [titleFiltering, genreFiltering]
+  );
   const favourites = upcomingMovies.filter((m) => m.favourite);
   localStorage.setItem("favourites", JSON.stringify(favourites));
 
@@ -16,18 +35,36 @@ const UpcomingMoviesPage: React.FC = () => {
     setUpcomingMovies(updatedMovies);
   };
 
+  const changeFilterValues = (type: string, value: string) => {
+    const changedFilter = { name: type, value: value };
+    const updatedFilterSet =
+      type === "title"
+        ? [changedFilter, filterValues[1]]
+        : [filterValues[0], changedFilter];
+    setFilterValues(updatedFilterSet);
+  };
+
   useEffect(() => {
     getUpcomingMovies().then((movies) => {
       setUpcomingMovies(movies);
     });
   }, []);
 
+  const displayedMovies = filterFunction(upcomingMovies);
+
   return (
-    <PageTemplate
-      title="Upcoming Movies"
-      movies={upcomingMovies}
-      selectFavourite={addToFavourites}
-    />
+    <>
+      <PageTemplate
+        title="Upcoming Movies"
+        movies={displayedMovies}
+        selectFavourite={addToFavourites}
+      />
+      <MovieFilterUI
+        onFilterValuesChange={changeFilterValues}
+        titleFilter={filterValues[0].value}
+        genreFilter={filterValues[1].value}
+      />
+    </>
   );
 };
 
