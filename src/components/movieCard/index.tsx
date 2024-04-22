eacf6884e61b7adb6bee13be1788212bf237bb7d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-22 21:16:55 +0100

    Update index.tsx

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -9,7 +9,6 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
-import Grid from "@mui/material/Grid";
 import img from "../../images/film-poster-placeholder.png";
 import { Link } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
