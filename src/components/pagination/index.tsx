f07d109457f628b07d75c9ff4e294ecdeb14d28e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-22 22:14:58 +0100

    Add CustomPagination component
    Implemented a CustomPagination component using Material-UI's Pagination component. This component provides pagination functionality for navigating between pages of content. It accepts props for currentPage, totalPages, and onPageChange, enabling seamless integration with parent components to manage pagination state. The component ensures a consistent user experience by displaying navigation controls such as first and last page buttons, and adjusts pagination appearance based on the provided page count and current page number. The CustomPagination component enhances the reusability and maintainability of pagination logic within React applications.

---
+++ src/components/pagination/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { Box } from "@mui/material";
+
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const CustomPagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  const handleChange = (_event: React.ChangeEvent<unknown>, value: number) => {
+    onPageChange(value); // Call the passed in onPageChange handler
+  };
+
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", padding: 3 }}>
+      <Stack spacing={3}>
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handleChange}
+          color="primary"
+          showFirstButton
+          showLastButton
+          siblingCount={1}
+          boundaryCount={1}
+          size="large"
+        />
+      </Stack>
+    </Box>
+  );
+};
+
+export default CustomPagination;
