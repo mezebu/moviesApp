48467a103a5fde199ee0f24cb1f308a714558a04
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-26 02:58:44 +0100

    Update index.tsx

--- src/components/headerMovieList/index.tsx
+++ src/components/headerMovieList/index.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
 const styles = {
   root: {
     display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    flexWrap: "wrap",
-    marginBottom: 1.5,
+    padding: 2,
+    justifyContent: "center",
+    marginBottom: 1,
   },
 };
 
@@ -24,16 +20,9 @@ const Header: React.FC<HeaderProps> = (props) => {
 
   return (
     <Paper variant="outlined" elevation={0} sx={styles.root}>
-      <IconButton aria-label="go back">
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
-
-      <Typography variant="h4" component="h3">
+      <Typography fontWeight="bold" variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton aria-label="go forward">
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
     </Paper>
   );
 };
