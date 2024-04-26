e8e23a41dbe49f630684a59a5c42cad73b9ad33d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-26 21:08:35 +0100

    Update interfaces for TV list page template, cast member, actor, and movie credits
    This commit includes updates to several interfaces:
    - Added TvListPageTemplateProps interface to define props for the TV list page template
    - Updated CastMember interface to include additional properties such as original_name, cast_id, character, and credit_id
    - Defined Actor interface to represent actor details including properties like also_known_as, biography, birthday, deathday, homepage, imdb_id, known_for_department, place_of_birth, and profile_path
    - Created MovieCredits interface to specify the structure of movie credits including id and an array of cast members
    
    Changes:
    - Added TvListPageTemplateProps interface
    - Updated CastMember interface with additional properties
    - Defined Actor interface to represent actor details
    - Created MovieCredits interface to specify movie credits structure

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -127,3 +127,40 @@ export interface TvListPageTemplateProps {
   shows: ListedTvShow[];
   action: (m: ShowT) => void;
 }
+
+export interface CastMember {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+
+export interface Actor {
+  adult: boolean;
+  also_known_as: string[];
+  biography: string;
+  birthday: string;
+  deathday: string | null;
+  gender: number;
+  homepage: string | null;
+  id: number;
+  imdb_id: string;
+  known_for_department: string;
+  name: string;
+  place_of_birth: string;
+  popularity: number;
+  profile_path: string | null;
+}
+
+export interface MovieCredits {
+  id: number;
+  cast: CastMember[];
+}
