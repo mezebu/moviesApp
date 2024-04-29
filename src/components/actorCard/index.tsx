d1e46beb178947ce3358409396fb232fa3038ebe
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-29 15:53:26 +0100

    Refactor ActorsCard component structure
    - Reorganized component structure for better readability and maintainability
    - Consolidated imports to improve code cleanliness
    - Implemented styled components for Card and CardMedia
    - Adjusted JSX layout and removed duplicate code
    - Updated prop usage for Card and CardMedia components
    - Enhanced typography styling for actor name and popularity
    - Optimized CardActions section and linked to actor details page

--- src/components/actorCard/index.tsx
+++ src/components/actorCard/index.tsx
@@ -1,17 +1,13 @@
 import React, { useContext } from "react";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardActions from "@mui/material/CardActions";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import { blue, red } from "@mui/material/colors";
-
-import { Actor } from "../../types/interfaces";
-import { MoviesContext } from "../../contexts/moviesContext";
-import InfoIcon from "@mui/icons-material/Info";
 import { Link } from "react-router-dom";
-import { Tooltip } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
+import CardActions from "@mui/material/CardActions";
+import { MoviesContext } from "../../contexts/moviesContext";
+import { Actor } from "../../types/interfaces";
+import { Content, StyledCard, StyledCardMedia } from "./styles";
 
 interface ActorListProps {
   actor: Actor;
@@ -26,36 +22,32 @@ const ActorsCard: React.FC<ActorListProps> = (props) => {
   if (favouriteActors.find((id) => id === actor.id)) actor.favourite = true;
 
   return (
-    <Card variant="outlined" elevation={0}>
-      <CardHeader
-        avatar={
-          <Avatar
-            sx={{ bgcolor: actor.gender === 1 ? red[500] : blue[500] }}
-            aria-label="recipe"
-          >
-            {actor.gender === 1 ? "F" : "M"}
-          </Avatar>
-        }
-        title={`${actor.name}`}
-        subheader={`Popularity: ${actor.popularity}`}
-      />
-      <CardMedia
-        component="img"
-        height="194"
+    <StyledCard variant="outlined" elevation={0}>
+      <StyledCardMedia
         image={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-        alt="Paella dish"
       />
-      <CardActions disableSpacing>
-        {props.action(actor)}
-        <Link to={`/actor/${actor.id}`}>
-          <Tooltip title="Actor Details">
-            <IconButton aria-label="share">
-              <InfoIcon fontSize="large" color="primary" />
-            </IconButton>
-          </Tooltip>
-        </Link>
-      </CardActions>
-    </Card>
+      <Content>
+        <Typography
+          variant="h6"
+          fontWeight="bold"
+          color="white"
+        >{`${actor.name}`}</Typography>
+        <Typography
+          variant="subtitle2"
+          color="white"
+        >{`Popularity: ${actor.popularity}`}</Typography>
+        <CardActions disableSpacing>
+          {props.action(actor)}
+          <Link to={`/actor/${actor.id}`}>
+            <Tooltip title="Actor Details">
+              <IconButton aria-label="share">
+                <InfoIcon fontSize="large" color="primary" />
+              </IconButton>
+            </Tooltip>
+          </Link>
+        </CardActions>
+      </Content>
+    </StyledCard>
   );
 };
 
