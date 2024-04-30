acad6783ffee7b15a8874ffb0597b3a8455cbe4d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-30 16:56:07 +0100

    Feat: Added sorting functionality for TV shows in TvShowsPage component
    Description:
    - Implemented sorting functionality in the TvShowsPage component to allow users to sort TV shows alphabetically.
    - Introduced a new state variable 'sortAsc' to toggle between ascending and descending sorting order.
    - Implemented the 'toggleSortOrder' function to switch between ascending and descending sorting.
    - Sorted the TV shows alphabetically based on the show's name.
    - Added a SortIcon floating action button for users to trigger the sorting action.

--- src/pages/tvShowsPage.tsx
+++ src/pages/tvShowsPage.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
-import { getTvShows } from "../api/tmdb-api";
 import { useQuery } from "react-query";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import Fab from "@mui/material/Fab";
 import Spinner from "../components/spinner";
 import TvListPageTemplate from "../components/templateTvListPage";
-import { BaseTVShow, DiscoverTvShows } from "../types/interfaces";
 import CustomPagination from "../components/pagination";
 import AddShowsToFavouritesIcon from "../components/cardIcons/addShowsToFavourites";
+import SortIcon from "@mui/icons-material/Sort";
+import { BaseTVShow, DiscoverTvShows } from "../types/interfaces";
+import { getTvShows } from "../api/tmdb-api";
+import { Tooltip } from "@mui/material";
 
 const TvShowsPage: React.FC = () => {
   const [page, setPage] = useState(1);
+  const [sortAsc, setSortAsc] = useState(true);
   const { data, error, isLoading, isError } = useQuery<DiscoverTvShows, Error>(
     ["tv", page],
     () => getTvShows(page),
     { keepPreviousData: true }
   );
 
-  const shows = data ? data.results : [];
+  const toggleSortOrder = () => {
+    setSortAsc(!sortAsc);
+  };
+
+  let shows = data ? data.results : [];
+  if (sortAsc) {
+    shows = [...shows].sort((a, b) => a.name.localeCompare(b.name));
+  } else {
+    shows = [...shows].sort((a, b) => b.name.localeCompare(a.name));
+  }
+
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   if (isLoading) {
     return <Spinner />;
@@ -39,6 +57,20 @@ const TvShowsPage: React.FC = () => {
         totalPages={data ? data.total_pages : 1}
         onPageChange={setPage}
       />
+      <Tooltip title="Sort Ascending Order">
+        <Fab
+          color="primary"
+          aria-label="sort"
+          onClick={toggleSortOrder}
+          style={{
+            position: "fixed",
+            bottom: matches ? 20 : 70,
+            right: 20,
+          }}
+        >
+          <SortIcon />
+        </Fab>
+      </Tooltip>
     </div>
   );
 };
