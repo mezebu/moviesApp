5eb754889bb145d28bed7e5e5ac6b3891243afcd
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-30 16:55:25 +0100

    Feat: Added sorting functionality for actors in ActorsPage component
    - Implemented sorting functionality in the ActorsPage component to allow users to sort actors alphabetically.
    - Introduced a new state variable 'sortAsc' to toggle between ascending and descending sorting order.
    - Implemented the 'toggleSortOrder' function to switch between ascending and descending sorting.
    - Sorted the actors alphabetically based on the actor's name.
    - Added a SortIcon floating action button for users to trigger the sorting action.

--- src/pages/actorsPage.tsx
+++ src/pages/actorsPage.tsx
@@ -2,18 +2,31 @@ import { useQuery } from "react-query";
 import { getActors } from "../api/tmdb-api";
 import PageTemplate from "../components/templateActorListPage";
 import { Actor, ActorResponse } from "../types/interfaces";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import Fab from "@mui/material/Fab";
 import Spinner from "../components/spinner";
 import { useState } from "react";
 import CustomPagination from "../components/pagination";
+import { Tooltip } from "@mui/material";
 import AddActorsToFavouritesIcon from "../components/cardIcons/addActorsToFavourites";
+import SortIcon from "@mui/icons-material/Sort";
 
 const ActorsPage: React.FC = () => {
   const [page, setPage] = useState(1);
+  const [sortAsc, setSortAsc] = useState(true);
   const { data, error, isLoading, isError } = useQuery<ActorResponse, Error>(
     ["actors", page],
     () => getActors(page),
     { keepPreviousData: true }
   );
+
+  const toggleSortOrder = () => {
+    setSortAsc(!sortAsc);
+  };
+
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   if (isLoading) {
     return <Spinner />;
@@ -23,7 +36,12 @@ const ActorsPage: React.FC = () => {
     return <h1>{error.message}</h1>;
   }
 
-  const actors = data ? data.results : [];
+  let actors = data ? data.results : [];
+  if (sortAsc) {
+    actors = [...actors].sort((a, b) => a.name.localeCompare(b.name));
+  } else {
+    actors = [...actors].sort((a, b) => b.name.localeCompare(a.name));
+  }
 
   return (
     <div>
@@ -39,6 +57,20 @@ const ActorsPage: React.FC = () => {
         totalPages={data ? data.total_pages : 1}
         onPageChange={setPage}
       />
+      <Tooltip title="Sort Ascending Order">
+        <Fab
+          color="primary"
+          aria-label="sort"
+          onClick={toggleSortOrder}
+          style={{
+            position: "fixed",
+            bottom: matches ? 20 : 70,
+            right: 20,
+          }}
+        >
+          <SortIcon />
+        </Fab>
+      </Tooltip>
     </div>
   );
 };
