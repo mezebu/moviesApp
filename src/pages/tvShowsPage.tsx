46a98f532f9ecfe710b950446d156d57e505bae8
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-04-30 20:39:24 +0100

    Refactor TvShowsPage component
    Refactored the TvShowsPage component for improved readability and maintainability. Incorporated useState hook for managing sorting criteria. Implemented sorting functionality based on various criteria such as name, vote average, and first air date. Introduced a compareShows function to compare shows based on the selected criteria and order. Added tooltips for sorting and toggle sort order buttons. Integrated a SortDialog component for displaying sorting options. Organized imports for better readability and removed redundant imports.

--- src/pages/tvShowsPage.tsx
+++ src/pages/tvShowsPage.tsx
@@ -11,29 +11,79 @@ import SortIcon from "@mui/icons-material/Sort";
 import { BaseTVShow, DiscoverTvShows } from "../types/interfaces";
 import { getTvShows } from "../api/tmdb-api";
 import { Tooltip } from "@mui/material";
+import UnfoldLessIcon from "@mui/icons-material/UnfoldLess";
+import SortDialog from "../components/SortDialog";
 
 const TvShowsPage: React.FC = () => {
   const [page, setPage] = useState(1);
+  const [open, setOpen] = useState(false);
   const [sortAsc, setSortAsc] = useState(true);
+  const [sortCriteria, setSortCriteria] = useState("name");
   const { data, error, isLoading, isError } = useQuery<DiscoverTvShows, Error>(
     ["tv", page],
     () => getTvShows(page),
     { keepPreviousData: true }
   );
 
-  const toggleSortOrder = () => {
-    setSortAsc(!sortAsc);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleSortCriteriaChange = (criteria: string) => {
+    setSortCriteria(criteria);
+    handleClose();
   };
+  const toggleSortOrder = () => setSortAsc(!sortAsc);
 
-  let shows = data ? data.results : [];
-  if (sortAsc) {
-    shows = [...shows].sort((a, b) => a.name.localeCompare(b.name));
-  } else {
-    shows = [...shows].sort((a, b) => b.name.localeCompare(a.name));
-  }
+  const sortOptions = [
+    { label: "Name", value: "name" },
+    { label: "Vote Average", value: "vote_average" },
+    { label: "First Air Date", value: "first_air_date" },
+  ];
 
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
+  const shows = data ? data.results : [];
+  // Function to compare based on the sort criteria
+  const compareShows = (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    a: any,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    b: any,
+    criteria: string,
+    ascending: boolean
+  ): number => {
+    if (
+      criteria === "vote_average" &&
+      typeof a.vote_average === "number" &&
+      typeof b.vote_average === "number"
+    ) {
+      return ascending
+        ? a.vote_average - b.vote_average
+        : b.vote_average - a.vote_average;
+    } else if (
+      criteria === "name" &&
+      typeof a.name === "string" &&
+      typeof b.name === "string"
+    ) {
+      return ascending
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name);
+    } else if (criteria === "first_air_date") {
+      const dateA = new Date(a.first_air_date);
+      const dateB = new Date(b.first_air_date);
+      // Check if dateA or dateB is invalid (Invalid Date will give NaN when calling getTime())
+      if (isNaN(dateA.getTime()) || isNaN(dateB.getTime())) {
+        return 0; // Could not compare dates, perhaps return 0 or handle differently
+      }
+      return ascending
+        ? dateA.getTime() - dateB.getTime()
+        : dateB.getTime() - dateA.getTime();
+    }
+    return 0; // Default case to handle unexpected criteria or data types
+  };
+
+  // Sorting the shows based on the selected criteria and order
+  shows.sort((a, b) => compareShows(a, b, sortCriteria, sortAsc));
 
   if (isLoading) {
     return <Spinner />;
@@ -57,18 +107,31 @@ const TvShowsPage: React.FC = () => {
         totalPages={data ? data.total_pages : 1}
         onPageChange={setPage}
       />
-      <Tooltip title="Sort Ascending Order">
+      <Tooltip title="Sort Tv Shows">
         <Fab
           color="primary"
           aria-label="sort"
-          onClick={toggleSortOrder}
-          style={{
-            position: "fixed",
-            bottom: matches ? 20 : 70,
-            right: 20,
-          }}
+          onClick={handleOpen}
+          style={{ position: "fixed", bottom: matches ? 90 : 140, right: 20 }}
         >
           <SortIcon />
+        </Fab>
+      </Tooltip>
+      <SortDialog
+        title="Sort Tv Shows"
+        open={open}
+        onClose={handleClose}
+        onSortChange={handleSortCriteriaChange}
+        sortOptions={sortOptions}
+      />
+      <Tooltip title="Toggle Sort Order">
+        <Fab
+          color="primary"
+          aria-label="toggle sort"
+          onClick={toggleSortOrder}
+          style={{ position: "fixed", bottom: matches ? 20 : 70, right: 20 }}
+        >
+          <UnfoldLessIcon />
         </Fab>
       </Tooltip>
     </div>
