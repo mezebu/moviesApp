9aa9538dda99a681d4d7e1177cb34ec68e2ba324
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-05-01 09:01:39 +0100

    Add Pagination Support to Query Results
    This commit adds support for pagination to the `queryResults` function, allowing users to retrieve search results across multiple pages. The `queryResults` function now accepts an additional parameter `page` to specify the page number of the search results. Pagination enhances the usability of the function, especially for large search queries, by fetching results in manageable chunks.

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -102,6 +102,24 @@ export const getUpcomingMovies = (page = 1) => {
     });
 };
 
+export const getSimilarMovies = (id: string, page: number = 1) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${
+      import.meta.env.VITE_TMDB_KEY
+    }&language=en-US&page=${page}`
+  )
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(
+          `Unable to fetch upcoming movies. Response status: ${response.status}`
+        );
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
 export const getTvShows = (page = 1) => {
   return fetch(
     `https://api.themoviedb.org/3/discover/tv?api_key=${
@@ -246,11 +264,11 @@ export const getActorDetails = (id: string) => {
     });
 };
 
-export const queryResults = (query: string) => {
+export const queryResults = (query: string, page: number = 1) => {
   return fetch(
     `https://api.themoviedb.org/3/search/multi?api_key=${
       import.meta.env.VITE_TMDB_KEY
-    }&language=en-US&include_adult=false&include_video=false&query=${query}`
+    }&language=en-US&page=${page}&include_adult=false&include_video=false&query=${query}`
   )
     .then((response) => {
       if (!response.ok)
