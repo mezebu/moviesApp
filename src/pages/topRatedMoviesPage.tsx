adbc426421b13c84daea99e62f28218dfa2e7a29
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-05-08 10:00:07 +0100

    Update topRatedMoviesPage.tsx

--- src/pages/topRatedMoviesPage.tsx
+++ src/pages/topRatedMoviesPage.tsx
@@ -60,6 +60,10 @@ const TopRatedMoviesPage: React.FC = () => {
         onFilterValuesChange={changeFilterValues}
         titleFilter={filterValues.find((f) => f.name === "title")?.value || ""}
         genreFilter={filterValues.find((f) => f.name === "genre")?.value || ""}
+        onSortChange={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        currentSort={""}
       />
       <CustomPagination
         currentPage={page}
