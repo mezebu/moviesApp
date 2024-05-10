d0e344531ebf3d6fc3d7052638429d3c516883ea
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-05-10 22:37:50 +0100

    Modify MovieReview component to display reviewer name
    This commit updates the MovieReview component to display the reviewer's name instead of the author's name if available. This change ensures consistency in displaying review information and accommodates cases where the author's name may not be provided.```

--- src/components/movieReview/index.tsx
+++ src/components/movieReview/index.tsx
@@ -4,7 +4,7 @@ import { Review } from "../../types/interfaces";
 const MovieReview: React.FC<Review> = (props) => {
   return (
     <>
-      <p>Review By: {props.author} </p>
+      <p>Review By: {props.author || props.reviewer_name} </p>
       <p>{props.content} </p>
     </>
   );
