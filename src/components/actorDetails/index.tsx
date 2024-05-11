65a64c4321858fa21c3f917d8c450179a67f3100
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2024-05-11 18:00:44 +0100

    Update index.tsx
    - Added components and functionality for displaying actor details including name, birthday, IMDb link, biography, also known as, and known for movies.

--- src/components/actorDetails/index.tsx
+++ src/components/actorDetails/index.tsx
@@ -1,20 +1,69 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
-import Divider from "@mui/material/Divider";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-
+import {
+  Box,
+  Chip,
+  Stack,
+  Divider,
+  Grid,
+  Typography,
+  Link,
+  Card,
+  CardActionArea,
+  CardMedia,
+} from "@mui/material";
 import { ActorProfileDetails } from "../../types/interfaces";
 import { StyledCard, StyledCardMedia } from "./styles";
+import { useQuery } from "react-query";
+import { getKnownFor } from "../../api/tmdb-api";
+import { useNavigate } from "react-router-dom";
 
 interface ActorDetailsProps {
   actor: ActorProfileDetails;
 }
 
+interface KnownForItem {
+  id: number;
+  poster_path: string;
+  title: string;
+}
 const ActorDetails: React.FC<ActorDetailsProps> = ({ actor }) => {
+  const navigate = useNavigate();
+  const { data: knownFor } = useQuery<KnownForItem[], Error>(
+    ["known_for", actor.id],
+    () => getKnownFor(`${actor.id}`)
+  );
+
+  const handleMovieClick = (movieId: number) => {
+    navigate(`/movies/${movieId}`);
+  };
+
+  // Function to compute age
+  const getAge = (birthday: string) => {
+    const birthDate = new Date(birthday);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
+  // Function to format the date
+  const formatDate = (date: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(date).toLocaleDateString("en-US", options);
+  };
+
+  // Handling non-available dates
+  const displayBirthday = actor.birthday
+    ? `${formatDate(actor.birthday)} (${getAge(actor.birthday)} years old)`
+    : "N/A";
+
   return (
     <Grid container>
       <Grid item lg={6} xs={12}>
@@ -32,11 +81,16 @@ const ActorDetails: React.FC<ActorDetailsProps> = ({ actor }) => {
             alignItems="center"
           >
             <Box>
-              <Typography gutterBottom variant="h5" fontWeight="bold">
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                fontWeight="bold"
+              >
                 {actor.name}
               </Typography>
               <Typography gutterBottom variant="subtitle2">
-                Birthday: {actor.birthday || "N/A"}
+                Birthday: {displayBirthday}
               </Typography>
               <Typography gutterBottom variant="subtitle2">
                 IMDb:{" "}
@@ -51,7 +105,6 @@ const ActorDetails: React.FC<ActorDetailsProps> = ({ actor }) => {
                 Died: {actor.deathday || "N/A"}
               </Typography>
             </Box>
-
             <Box>
               <Typography gutterBottom variant="subtitle2">
                 Gender: {actor.gender === 1 ? "Female" : "Male"}
@@ -76,13 +129,16 @@ const ActorDetails: React.FC<ActorDetailsProps> = ({ actor }) => {
           </Stack>
           <Divider />
           <Typography variant="body2" style={{ marginTop: 12 }}>
-            Biography: {actor.biography}
+            <Typography component="span" variant="subtitle2" fontWeight="bold">
+              Biography:
+            </Typography>{" "}
+            {actor.biography}
           </Typography>
         </Box>
 
         <Box sx={{ p: 2 }}>
           <Box>
-            <Typography gutterBottom variant="subtitle2">
+            <Typography gutterBottom variant="subtitle2" fontWeight="bold">
               Also known as: {actor.also_known_as.join(", ")}
             </Typography>
           </Box>
@@ -95,7 +151,29 @@ const ActorDetails: React.FC<ActorDetailsProps> = ({ actor }) => {
               View IMDb Profile
             </Link>
           </Typography>
-          <Typography gutterBottom variant="body2">
+          <Typography gutterBottom variant="body2" fontWeight="bold">
+            Known for:
+          </Typography>
+          <Stack direction="row" spacing={1}>
+            {knownFor &&
+              knownFor.slice(0, 5).map((k: KnownForItem) => (
+                <Box key={k.id} onClick={() => handleMovieClick(k.id)}>
+                  <Card elevation={0}>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="190"
+                        image={`https://image.tmdb.org/t/p/w500${k.poster_path}`}
+                        alt={k.title}
+                      />
+                    </CardActionArea>
+                  </Card>
+                  <Typography variant="caption">{k.title}</Typography>
+                </Box>
+              ))}
+          </Stack>
+
+          <Typography gutterBottom variant="body2" fontWeight="bold">
             Adult
           </Typography>
           <Stack direction="row" spacing={1}>
